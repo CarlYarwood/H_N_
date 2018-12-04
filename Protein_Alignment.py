67f3285427fd3554e6e8dd26b710f02768b5b6dd
Jonathan Rogers <jpr8237@truman.edu>
2018-12-04 10:47:14 -0600

    Began the protein alignment program. Added a dictionary with all codon combinations.

---
+++ Protein_Alignment.py
@@ -0,0 +1,25 @@
+protein_dict = {'TTT':'Phe','TTC':'Phe','TTA':'Leu','TTG':'Leu','TCT':'Ser',
+                'TCC':'Ser','TCA':'Ser','TCG':'Ser','TAT':'Tyr','TAC':'Tyr',
+                'TAA':'Ter','TAG':'Ter','TGT':'Cys','TGC':'Cys','TGA':'Ter',
+                'TGG':'Trp','CTT':'Leu','CTC':'Leu','CTA':'Leu','CTG':'Leu',
+                'CCT':'Pro','CCA':'Pro','CCG':'Pro','CCC':'Pro','CAT':'His',
+                'CAC':'His','CAA':'Gln','CAG':'Gln','CGT':'Arg','CGC':'Arg',
+                'CGA':'Arg','CGG':'Arg','ATT':'Ile','ATA':'Ile','ATC':'Ile',
+                'ATG':'Met','ACT':'Thr','ACA':'Thr','ACC':'Thr','ACG':'Thr',
+                'AAT':'Asn','AAC':'Asn','AAA':'Ser','AAG':'Ser','AGA':'Arg',
+                'AGG':'Arg','GTT':'Val','GTA':'Val','GTC':'Val','GTG':'Val',
+                'GCT':'Ala','GCC':'Ala','GCA':'Ala','GCG':'Ala','GAT':'Asp',
+                'GAC':'Asp','GAA':'Glu','GAG':'Glu','GGT','Gly','GGC','Gly',
+                'GGA','Gly','GGG','Gly',}
+
+
+
+def main():
+    convert_bases_to_proteins
+
+
+
+
+
+
+if __name__ == '__main__': main()
